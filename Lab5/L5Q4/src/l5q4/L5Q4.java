e156510887d856864bfe5d53c4244e730095ea51
jitesh1059 <25006745@siswa.um.edu.my>
2025-11-11 11:33:13 +0800

    l5q4 complete

---
+++ Lab5/L5Q4/src/l5q4/L5Q4.java
@@ -0,0 +1,16 @@
+/*
+Write a program that rotates 90 degrees clockwise a 3 by 3 matrix
+ */
+package l5q4;
+import java.util.Random;
+/**
+ *
+ * @author jites
+ */
+public class L5Q4 {
+    public static void main(String[] args) {
+        // Setup object imports
+        
+    }
+    
+}
